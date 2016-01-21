ddecc598238dcaf431c1ecf9bfc77166ca0fa7eb
smugunicorn <huangyt1012@gmail.com>
2016-01-20 23:20:57 -0800

    new updates on tf_binding.sh

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,17 +1,15 @@
-#PBS tags
 #!/bin/csh
 #PBS -q hotel
-#PBS -N tf_binding.sh
+#PBS -N YHuang_MHunt_biom262_HW1
 #PBS -l nodes=10:ppn=2
-#PBS -l walltime=0:50:00
-#PBS -o tf_binding.sh.o######
-#PBS -e tf_binding.sh.e######
+#PBS -l walltime=0:10:00
+#PBS -o tf_binding.sh.o
+#PBS -e tf_binding.sh.e
 #PBS -V
-#PBS -M yoh006@ucsd.edu
+#PBS -M yoh006@ucsd.edu 
 #PBS -m abe
 #PBS -A ucsd-train42
-cd /oasis/tscc/scratch/ucsd-train42
-mpirun -v -machinefile $PBS_NODEFILE -np 20 <./mpi.out>
+cd ~/code/biom262/weeks/week01/data2
 
 Exercise 1 
 grep "NFKB" tf.bed > tf.nfkb.bed
@@ -23,4 +21,12 @@ Exercise 3
 module load biotools
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
 
+
+
+
+
+
+
+
+echo "Hello I am a message in standard out (stdout)"
 
