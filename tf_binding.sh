d3a7b066318ac5ca49c793b52b5f581b89716d2a
Matt Hunt <mhunt2488@gmail.com>
2016-01-21 09:36:10 -0800

    Added 3-6

--- tf_binding.sh
+++ tf_binding.sh
@@ -11,18 +11,24 @@
 #PBS -A ucsd-train42
 cd ~/code/biom262/weeks/week01/data2
 
-Exercise 1 
+#Exercise 1 
 grep "NFKB" tf.bed > tf.nfkb.bed
 
-Exercise 2
+#Exercise 2
 awk '{if ($3 == "transcript")print}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
 
-Exercise 3
+#Exercise 3
 module load biotools
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
 
+#Exercise 4 
+bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
 
+#Exercise 5
+bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -s -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
 
+#Exercise 6
+### we used grep -F -f nfkb.paper.fasta gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta > ex6.out.fasta. Overall in looking at the data that Wong et al. generated looks pretty similar to ours. 462 of the 11mer's from their "cannonical" NFKB binding results overlay with our outputs (2442). However I could find none of the "non-cannonical" motifs they showed overlapping with our dataset. This shows why sometimes wetlab work must be done to verify bioinformatically ontained inferences. 
 
 
 
@@ -30,3 +36,4 @@ bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 
 
 echo "Hello I am a message in standard out (stdout)"
 
+echo "Hello I am a message in standard error (stderr) >&2"
