9cc119272a8b51b77bd208dea09ea81fe0e1078d
smugunicorn <huangyt1012@gmail.com>
2016-01-20 18:55:43 -0800

    codes for exercise 1-3

--- tf_binding.sh
+++ tf_binding.sh
@@ -12,3 +12,14 @@
 cd /oasis/tscc/scratch/<user name>
 mpirun -v -machinefile $PBS_NODEFILE -np 20 <./mpi.out>
 
+Exercise 1 
+grep "NFKB" tf.bed > tf.nfkb.bed
+
+Exercise 2
+awk '{if ($3 == "transcript")print}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
+
+Exercise 3
+module load biotools
+bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
+
+
